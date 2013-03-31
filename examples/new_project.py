73d63ca58965857b6a2b2ba50526c87467f55bd6
Matthew Radway <mradway@gmail.com>
2013-03-31 01:19:04 -0600

    Added an example for new_project (Issue #30).

---
+++ examples/new_project.py
@@ -0,0 +1,13 @@
+import hycohanz as hfss
+
+raw_input('Press "Enter" to connect to HFSS.>')
+
+[oAnsoftApp, oDesktop] = hfss.setup_interface()
+
+raw_input('Press "Enter" to create a new project.>')
+
+oProject = hfss.new_project()
+
+raw_input('Press "Enter" to quit HFSS.>')
+
+hfss.quit_application(oDesktop)
