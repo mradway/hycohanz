9a622ef9b1f4f8dc4fe8a0f3d4af4826e902b671
Matthew Radway <mradway@gmail.com>
2013-03-31 01:34:12 -0600

    Added an example for insert_design() (Issue #31).

---
+++ examples/insert_design.py
@@ -0,0 +1,17 @@
+import hycohanz as hfss
+
+raw_input('Press "Enter" to connect to HFSS.>')
+
+[oAnsoftApp, oDesktop] = hfss.setup_interface()
+
+raw_input('Press "Enter" to create a new project.>')
+
+oProject = hfss.new_project()
+
+raw_input('Press "Enter" to insert a new DrivenModal design named HFSSDesign1.>')
+
+oDesign = hfss.insert_design(oProject, "HFSSDesign1", "DrivenModal")
+
+raw_input('Press "Enter" to quit HFSS.>')
+
+hfss.quit_application(oDesktop)
