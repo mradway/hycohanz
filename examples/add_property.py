276c99067f60b97cdfe85e4eab3642aaff2436db
Matthew Radway <mradway@gmail.com>
2013-04-03 23:48:02 -0600

    Implemented an interface to the ChangeProperty() function named add_property() (closes #22), including an example.  This example also shows basic usage of the Expression object (closes #38).

---
+++ examples/add_property.py
@@ -0,0 +1,21 @@
+import hycohanz as hfss
+
+raw_input('Press "Enter" to connect to HFSS.>')
+
+[oAnsoftApp, oDesktop] = hfss.setup_interface()
+
+raw_input('Press "Enter" to create a new project.>')
+
+oProject = hfss.new_project(oDesktop)
+
+raw_input('Press "Enter" to insert a new DrivenModal design named HFSSDesign1.>')
+
+oDesign = hfss.insert_design(oProject, "HFSSDesign1", "DrivenModal")
+
+raw_input('Press "Enter" to insert a new property into the design named "length", having a value of "1m".>')
+
+hfss.add_property(oDesign, "length", hfss.Expression("1m"))
+
+raw_input('Press "Enter" to quit HFSS.>')
+
+hfss.quit_application(oDesktop)
--- hycohanz/hycohanz.py
+++ hycohanz/hycohanz.py
@@ -428,3 +428,32 @@ def insert_analysis_setup(oDesign,
                                 "NoAdditionalRefinementOnImport:=", NoAdditionalRefinementOnImport])
                                 
     return Name
+
+def add_property(oDesign, name, value):
+    """
+    Add a design property.
+    
+    Parameters
+    ----------
+    oDesign : pywin32 COMObject
+        The HFSS design from which to retrieve the module.
+    name : str
+        The name of the property to add.
+    value : Hyphasis Expression object
+        The value of the property.
+        
+    Returns
+    -------
+    None
+    
+    """
+    propserversarray = ["NAME:PropServers", "LocalVariables"]
+
+    newpropsarray = ["NAME:NewProps", ["NAME:" + name, 
+                                       "PropType:=", "VariableProp", 
+                                       "UserDef:=", True, 
+                                       "Value:=", Expression(value).expr]]
+    
+    proptabarray = ["NAME:LocalVariableTab", propserversarray, newpropsarray]
+          
+    oDesign.ChangeProperty(["NAME:AllTabs", proptabarray])
