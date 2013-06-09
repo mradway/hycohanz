262122ee682dacf640e693a83f332216a8b6a37f
Matthew Radway <mradway@gmail.com>
2013-06-09 04:53:41 -0600

    Implemented wrapper for ClcEval(), with an example (closes #108).

--- hycohanz/fieldscalculator.py
+++ hycohanz/fieldscalculator.py
@@ -44,6 +44,46 @@ def calc_op(oFieldsReporter, OperationString):
     
 
 
+def clc_eval(oFieldsReporter, setupname, sweepname, freq, phase, variablesdict):
+    """
+    Evaluates the expression at the top of the Fields Calculator stack using 
+    the provided solution name and variable values.
+
+    Parameters
+    ----------
+    oFieldsReporter : pywin32 COMObject
+        An HFSS "FieldsReporter" module 
+    setupname : str
+        Name of HFSS setup to use, for example "Setup1"
+    sweepname : str
+        Name of HFSS sweep to use, for example "LastAdaptive"
+    freq : float
+        Frequency at which to evaluate the calculator expression.
+    phase : float
+        Phase in degrees at which to evaluate the calculator expression.
+    variablesdict : dict
+        Dictionary listing the variables and their values that define the 
+        design variation, except for 'Freq' and 'Phase'.  Variable names are the keys (strings), and the values are 
+        the values of the variable (floats). For example: {'radius': 0.5, 'height': '2.0'}
+        
+    Returns
+    -------
+    None
+    """
+    solutionname = setupname + " : " + sweepname
+    
+    variablesarray = ["Freq:=", str(freq) + 'Hz', "Phase:=", str(phase) + 'deg']
+    
+    for key in variablesdict:
+        variablesarray += [str(key) + ':=', str(variablesdict[key])]
+        
+    print('solutionname: ' + str(solutionname))
+    print('variablesarray: ' + str(variablesarray))
+    
+    oFieldsReporter.ClcEval(solutionname, variablesarray)
+    
+    
+    
 def enter_qty(oFieldsReporter, FieldQuantityString):
     """
     Enter a field quantity.
