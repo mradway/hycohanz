74c01a816f98c9664d9c5ea3382a5b164629cde0
Matthew Radway <mradway@gmail.com>
2013-06-09 04:51:53 -0600

    Implemented wrapper for EnterVol(), with an example (closes #107).

--- hycohanz/fieldscalculator.py
+++ hycohanz/fieldscalculator.py
@@ -6,6 +6,25 @@ HFSS Fields Calculator functions.
 
 from __future__ import division, print_function, unicode_literals, absolute_import
 
+def enter_vol(oFieldsReporter, VolumeName):
+    """
+    Enters a volume defined in the 3D Modeler editor into the Fields Calculator.
+
+    Parameters
+    ----------
+    oFieldsReporter : pywin32 COMObject
+        An HFSS "FieldsReporter" module
+    VolumeName : str
+        Name of a volume defined in the 3D Modeler editor.
+
+    Returns
+    -------
+    None
+    """
+    oFieldsReporter.EnterVol(VolumeName)
+
+
+
 def calc_op(oFieldsReporter, OperationString):
     """
     Performs a calculator operation in the Fields Calculator.
