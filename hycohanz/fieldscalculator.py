37ef2949d8fd5342165d3cc20a91df447a9591c1
Matthew Radway <mradway@gmail.com>
2013-06-09 04:48:34 -0600

    Implemented wrapper for CalcOp(), along with an example (closes #100).

--- hycohanz/fieldscalculator.py
+++ hycohanz/fieldscalculator.py
@@ -6,6 +6,25 @@ HFSS Fields Calculator functions.
 
 from __future__ import division, print_function, unicode_literals, absolute_import
 
+def calc_op(oFieldsReporter, OperationString):
+    """
+    Performs a calculator operation in the Fields Calculator.
+    
+    Parameters
+    ----------
+    oFieldsReporter : pywin32 COMObject
+        An HFSS "FieldsReporter" module 
+    OperationString : str
+        The text on the corresponding calculator button.
+        
+    Returns
+    -------
+    None
+    """
+    oFieldsReporter.CalcOp(OperationString)
+    
+
+
 def enter_qty(oFieldsReporter, FieldQuantityString):
     """
     Enter a field quantity.
