32bcad421aa994ae148a6e7626ab9160e8201926
Matthew Radway <mradway@gmail.com>
2013-06-24 21:30:20 -0600

    Fixed an issue that caused the example for assign_material() to crash, as well as cleaning up some of its code (fixes #114).

--- examples/assign_material.py
+++ examples/assign_material.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import hycohanz as hfss
 
 raw_input('Press "Enter" to connect to HFSS.>')
@@ -16,21 +18,23 @@ raw_input('Press "Enter" to set the active editor to "3D Modeler" (The default a
 
 oEditor = hfss.set_active_editor(oDesign)
 
-raw_input('Press "Enter" to insert some circle properties into the design.>')
+raw_input('Press "Enter" to insert some sphere properties into the design.>')
 
 hfss.add_property(oDesign, "xcenter", hfss.Expression("1m"))
 hfss.add_property(oDesign, "ycenter", hfss.Expression("2m"))
 hfss.add_property(oDesign, "zcenter", hfss.Expression("3m"))
 hfss.add_property(oDesign, "diam", hfss.Expression("1m"))
 
-raw_input('Press "Enter" to draw a circle using the properties.>')
+raw_input('Press "Enter" to draw a sphere using the properties.>')
 
-sphere1 = hfss.create_sphere(oEditor,   hfss.Expression("xcenter"), 
-										hfss.Expression("ycenter"), 
-										hfss.Expression("zcenter"), 
-										hfss.Expression("diameter")/2)
+sphere1 = hfss.create_sphere(
+    oEditor,   
+    hfss.Expression("xcenter"), 
+    hfss.Expression("ycenter"), 
+    hfss.Expression("zcenter"), 
+    hfss.Expression("diam")/2)
 
-raw_input("Press "Enter" to change the circle's material to copper>")
+raw_input('''Press "Enter" to change the sphere's material to copper>''')
 
 hfss.assign_material(oEditor, [sphere1], MaterialName="copper")
 
