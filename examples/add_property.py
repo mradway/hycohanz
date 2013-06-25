834d0b41c1a24452ac18551a4ab614593a6543cc
Matthew Radway <mradway@gmail.com>
2013-06-24 19:32:04 -0600

    Changed example for add_property() to use context managers.

--- examples/add_property.py
+++ examples/add_property.py
@@ -2,25 +2,16 @@ import hycohanz as hfss
 
 raw_input('Press "Enter" to connect to HFSS.>')
 
-[oAnsoftApp, oDesktop] = hfss.setup_interface()
-
-raw_input('Press "Enter" to create a new project.>')
-
-oProject = hfss.new_project(oDesktop)
-
-raw_input('Press "Enter" to insert a new DrivenModal design named HFSSDesign1.>')
-
-oDesign = hfss.insert_design(oProject, "HFSSDesign1", "DrivenModal")
-
-raw_input('Press "Enter" to insert a new property into the design named "length", having a value of "1m".>')
-
-hfss.add_property(oDesign, "length", hfss.Expression("1m"))
-
-raw_input('Press "Enter" to quit HFSS.>')
-
-hfss.quit_application(oDesktop)
-
-del oDesign
-del oProject
-del oDesktop
-del oAnsoftApp
+with hfss.App() as App:
+    raw_input('Press "Enter" to create a new project.>')
+    
+    with hfss.NewProject(App.oDesktop) as P:
+        
+        raw_input('Press "Enter" to insert a new DrivenModal design named HFSSDesign1.>')
+        
+        with hfss.InsertDesign(P.oProject, "HFSSDesign1", "DrivenModal") as D:
+            raw_input('Press "Enter" to insert a new property into the design named "length", having a value of "1m".>')
+            
+            hfss.add_property(D.oDesign, "length", hfss.Expression("1m"))
+            
+            raw_input('Press "Enter" to quit HFSS.>')
