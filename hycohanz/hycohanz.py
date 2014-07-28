55d5b64b96f8c3ad379e3a1819e805f562378f23
Matthew Radway <mradway@gmail.com>
2014-07-28 02:19:28 -0700

    Instead of importing * from the material module, each function is now imported explicitly.  This makes static code analysis and introspection tools work better.

--- hycohanz/hycohanz.py
+++ hycohanz/hycohanz.py
@@ -42,7 +42,9 @@ from hycohanz.design import (get_module,
 
 from hycohanz.expression import Expression
 from hycohanz.modeler3d import *
-from hycohanz.material import *
+from hycohanz.material import ( add_material,
+                                does_material_exist,
+                                )
 
 from hycohanz.analysis_setup import (insert_frequency_sweep, 
                                      insert_analysis_setup)
